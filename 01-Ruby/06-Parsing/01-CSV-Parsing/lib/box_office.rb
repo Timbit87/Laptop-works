de5936a967b01f33d4eb7eb7d13ea098f1315158
Andrew Beckett <Andrewbeckett95@gmail.com>
2024-05-02 09:16:27 +0900

    Solved

--- 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
+++ 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
@@ -4,12 +4,9 @@ def most_successful(number, max_year, file_path)
   # TODO: return an array of most successful movies before `max_year`
   return_array = []
   lines = CSV.read(file_path)
-  sorted_lines = lines.sort_by{|line| line[2].to_i}.reverse
+  sorted_lines = lines.sort_by { |line| line[2].to_i }.reverse
   sorted_lines.each do |csv|
-    return_array << { name: csv[0], year: csv[1].to_i, earnings: csv[2].to_i }
+    return_array << { name: csv[0], year: csv[1].to_i, earnings: csv[2].to_i } if csv[1].to_i < max_year
   end
   return_array.first(number)
 end
-
-
-  #CSV.foreach(file_path, headers: false, col_sep: ",") do |csv|
